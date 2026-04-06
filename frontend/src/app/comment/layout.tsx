61fa751caffe0d1cec9046c890fe1bf045584e99
박진완 <wlsdhks5495@naver.com>
2026-04-06 16:54:45 +0900

    fix(frontend): remove unused PageTitle import from comment layout

--- frontend/src/app/comment/layout.tsx
+++ frontend/src/app/comment/layout.tsx
@@ -1,5 +1,4 @@
 import PageContainer from "@/components/PageContainer";
-import PageTitle from "@/components/PageTitle";
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
